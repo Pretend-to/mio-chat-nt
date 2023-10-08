406c2b11c59b392a8bc53d48439ce6332edb9855
Mio-FCIP <1099834705@qq.com>
2023-10-09 00:55:41 +0800

    update css

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -79,7 +79,7 @@ export default {
                 </div>
             </div>
             <div class="down-half">
-                <a href="https://github.com" target="_blank" class="side-icon">
+                <a href="https://github.com/Pretend-to/mio-chat-nt" target="_blank" class="side-icon">
                     <svg t="1695150961459" class="icon" viewBox="0 0 1024 1024" version="1.1"
                         xmlns="http://www.w3.org/2000/svg" p-id="16393">
                         <path
