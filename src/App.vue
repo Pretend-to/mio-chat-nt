48e76bcfa4b8aa3538c87cbd7b687cdf8a72f237
Mio-FCIP <1099834705@qq.com>
2023-10-10 22:57:21 +0800

    mobile 0.13

--- src/App.vue
+++ src/App.vue
@@ -20,7 +20,6 @@ export default{
 
 <style>
 @media (max-width: 600px) {
-  #sidebar,
   #chatwindow {
     display: none;
   }
@@ -28,6 +27,30 @@ export default{
     flex-basis: 100%;
     max-width: 100%;
   }
-
+  #app{
+    flex-direction: column-reverse;
+  }
+  #sidebar{
+    height: 48px;
+    min-width: 100%;
+    flex-direction: row;
+  }
+  #sidebar .avatar{
+    display: none;
+  }
+  .options#side{
+    flex-direction: row;
+  }
+  .options#side .up-half{
+    padding-top: 0px;
+  }
+  .options#side .up-half,
+  .options#side .down-half{
+    padding-top: 24px;
+    flex-basis: 50%;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+  }
 }
 </style>
